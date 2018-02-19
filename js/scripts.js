a11005b9698c72bc28b678bc5bc9533f38cb86f9
MikeBLambert <mlamb128@gmail.com>
2018-02-18 20:43:18 -0800

    add JS and jquery which total up price and pizza details and displays to user in HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,110 @@
 //Business Logic
+//Object for details of each pizza that is ordered.
 function Pizza (size,toppings) {
   this.size = size;
   this.toppings = toppings;
   };
 
-Pizza.prototype.sizePrice = function() {
-  if (this.size == "small") {
-    return 7;
-  } else if (this.size == "medium") {
-    return 10;
-  } else {
-    return 13;
-  }
-};
+//Object that stores the user's information
+function UserInfo (firstName,lastName,streetAddress,city,state,zip) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.streetAddress = streetAddress;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.userPizza = [];
+    this.userPrice = [];
+  };
 
-Pizza.prototype.toppingPrice = function() {
-  var priceForToppings = 0;
-  console.log(this.toppings);
+Pizza.prototype.pizzaPrice = function() {
+  var pizzaPrice = 0
+  if (this.size == "small") {
+    pizzaPrice += 7;
+  } else if (this.size == "medium") {
+    pizzaPrice += 10;
+  } else if (this.size == "large") {
+    pizzaPrice +=13
+  }
   for (i=0; i<this.toppings.length; i+=1) {
     if (this.toppings[i] === "cheese") {
-      priceForToppings += 1
-      console.log(priceForToppings);
+      pizzaPrice += 1
     } else if (this.toppings[i] === "pepperoni") {
-      priceForToppings += 2
-      console.log(priceForToppings);
+      pizzaPrice += 2
     } else if (this.toppings[i] ==="olives") {
-      priceForToppings += 1
+      pizzaPrice += 1
     } else if (this.toppings[i] === "peppers") {
-      priceForToppings += 2
+      pizzaPrice += 2
     }
-  }
-  return priceForToppings;
+    return pizzaPrice;
+  };
 };
+
+var totalPrice = 0
+
+// Pizza.prototype.toppingPrice = function() {
+//   var priceForToppings = 0;
+//   for (i=0; i<this.toppings.length; i+=1) {
+//     if (this.toppings[i] === "cheese") {
+//       priceForToppings += 1
+//     } else if (this.toppings[i] === "pepperoni") {
+//       priceForToppings += 2
+//     } else if (this.toppings[i] ==="olives") {
+//       priceForToppings += 1
+//     } else if (this.toppings[i] === "peppers") {
+//       priceForToppings += 2
+//     }
+//   }
+//   return priceForToppings;
+// };
+
 
 
 //User Interface
 $(document).ready(function() {
   $("#addPizza").click(function() {
-  event.preventDefault();
+    event.preventDefault();
     var size = $("input:radio[name=size]:checked").val();
     var toppings = [];
     $("input:checkbox[name=toppings]:checked").each(function() {
       toppings.push($(this).val());
     });
     var pizza = new Pizza(size,toppings);
-    var price = pizza.toppingPrice() + pizza.sizePrice();
+    var price = pizza.pizzaPrice();
 
+
+
+    // var price = pizza.toppingPrice() + pizza.sizePrice();
     document.getElementById("pizzaInfoForm").reset();
-    $("#displayOrderInfo").append("<h1>Shopping Cart:</h1>" +" One " + pizza.size+" pizza with the following toppings: "+ "<li>"+pizza.toppings+"</li>"+"This pizza will cost $"+price);
+    $("#shoppingCart").show();
+    $("button#submitOrder").show();
+    $("#userInfoForm").show();
+    $("#displayOrderInfo").append(
+      "<h3><li>One " + pizza.size + " pizza. <br>Toppings: <ul>" + pizza.toppings.join("<br>") +"</ul>" + "Price: $" + price + "</li></h3>");
+    totalPrice += price
+    console.log(totalPrice);
+    });
+  $("#submitOrder").click(function() {
+    event.preventDefault();
+    var firstName = $("#user-first-name").val();
+    var lastName = $("#user-last-name").val();
+    var streetAddress = $("#user-street-address").val();
+    var city = $("#user-city").val();
+    var state = $("#user-state").val();
+    var zip = $("#user-zip").val();
+    var userInfo = new UserInfo(firstName,lastName,streetAddress,city,state,zip);
+    $("#userInfoForm").hide();
+    $("#pizzaInfoForm").hide();
+    $("#finalOrderDisplay").show();
+    $("#finalOrderInfo").append(
+      userInfo.firstName + " " + userInfo.lastName +
+      "<br>" +
+      userInfo.streetAddress +
+      "<br>" +
+      userInfo.city + ", " + userInfo.state + " " + userInfo.zip +
+      "<br>" +
+      "The total amount due is $" + totalPrice
+
+    );
   });
 });
